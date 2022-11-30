5dad3b245aa05de38b412a08bb4ce2d244aec8c7
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2022-11-30 17:00:00 +0100

    switch browserRouter postion in App.js

--- src/App.js
+++ src/App.js
@@ -7,17 +7,18 @@ import Products from './pages/Products';
 
 function App() {
   return (
-    <BrowserRouter>
+
       <div className="w-full h-screnn">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/Services" element={<Services />}></Route>
-          <Route path="/Products" element={<Products />}></Route>
-          <Route path="/Login" element={<Login />}></Route>
-        </Routes>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/Services" element={<Services />}></Route>
+            <Route path="/Products" element={<Products />}></Route>
+            <Route path="/Login" element={<Login />}></Route>
+          </Routes>
+        </BrowserRouter> 
       </div>
-    </BrowserRouter>
   );
 }
 
