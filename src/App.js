f3c0db97ef7ae644d352c16298f4a32d1f77c9b1
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2022-11-30 15:11:54 +0100

    set up routes and Links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import Navbar from './components/Navbar';
 import Login from './pages/Login';
+import {BrowserRouter ,Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-        <Login />
-    </div>
+    <BrowserRouter>
+      <div className="w-full h-screnn">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Services" element={<Services />}></Route>
+          <Route path="/Products" element={<Products />}></Route>
+          <Route path="/Login" element={<Login />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
-    <div>
-        
+    <div className="flex justify-between w-full p-4 text-sm font-semibold shadow-lg shadow-gray-200 bg-form-color">
+      <img src="" alt="" />
+      <ul className="flex px-2 text-white gap-9">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Services">Services</Link>
+        </li>
+        <li>
+          <Link to="/Products">Products</Link>
+        </li>
+        <li>
+          <Link to="/Login">Login</Link>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
