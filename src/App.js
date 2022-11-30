2c2778336c99970d403ec53b748100c431052a5c
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2022-11-30 15:14:50 +0100

    create pages files and connect them with routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import {BrowserRouter ,Routes, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import Services from './pages/Services';
+import Products from './pages/Products';
 
 function App() {
   return (
@@ -8,7 +11,7 @@ function App() {
       <div className="w-full h-screnn">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<HomePage />}></Route>
           <Route path="/Services" element={<Services />}></Route>
           <Route path="/Products" element={<Products />}></Route>
           <Route path="/Login" element={<Login />}></Route>
